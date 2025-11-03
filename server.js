2343c310f7ca6ea9d1827bd52c85f3ae8579f34b
Yoonus Anees <yoonusaneesniis@gmail.com>
2025-11-04 02:06:17 +0530

    fix(Corse Origin)

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173", //  your frontend origin
-  credentials: true,               //  allow cookies/headers
+  origin: [
+    "http://localhost:5173",
+    "https://ems-fe-teal.vercel.app"
+  ],
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Serve uploaded files
 
